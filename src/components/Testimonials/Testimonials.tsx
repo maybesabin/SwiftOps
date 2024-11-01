66bee872cf47a4b0ffb9bb5cc0fd6ece4ac05e12
maybesabin <highsabin987@gmail.com>
2024-11-01 19:52:48 +0545

    minor fixes

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -2,8 +2,8 @@ import TestimonialHeader from "./TestimonialHeader"
 
 const Testimonials = () => {
     return (
-        <div id="testimonials" className="md:py-[7rem] py-12 -mt-1 w-screen relative dark:bg-[#111214] bg-white flex flex-col items-center gap-12">
-            <h1 className="pb-6 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white font-semibold text-3xl md:text-6xl md:px-0 sm:px-9 px-4 md:leading-[4.5rem]">
+        <div id="testimonials" className="md:py-[7rem] py-12 -mt-1 w-screen relative dark:bg-[#111214] bg-white flex flex-col items-center md:gap-12 gap-9">
+            <h1 className="md:pb-6 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white font-semibold text-3xl md:text-6xl md:px-0 sm:px-9 px-4 md:leading-[4.5rem]">
                 Trusted by <span className="dark:text-white text-[#169d73]">millions</span> <br className="md:hidden flex" /> of people
             </h1>
             <TestimonialHeader />
